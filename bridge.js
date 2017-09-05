accce74ec446cc0a21884b213a2c34f2b7ba59a2
agapebc <31616429+agapebc@users.noreply.github.com>
2017-09-05 10:57:28 +0300

    Add files via upload

---
+++ bridge.js
@@ -0,0 +1,20 @@
+<head>
+    <meta charset="utf-8">
+
+    <title>test run app</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+<link rel="stylesheet" href="agp/agape.css">
+
+<script src="agp/Bridge.js"></script>
+
+    <script>
+ 	WebView myWebView = (WebView) findViewById(R.id.webview);
+ 	WebSettings webSettings = myWebView.getSettings();
+ 	webSettings.setJavaScriptEnabled(true);
+    
+
+	</script>
+
+</head>
